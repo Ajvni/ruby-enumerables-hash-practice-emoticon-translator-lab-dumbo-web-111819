f722677a617a28428e3006152e7ff5bdc0061421
Ajani Queeley <ajaniqueeley@gmail.com>
2019-11-15 17:52:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,7 @@ def get_japanese_emoticon(file, emoticon)
   result ? result : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, emoticon)
+  result = load_library(file)['get_meaning'][emoticon]
+  result ? result : "Sorry, that emoticon was not found"
 end
